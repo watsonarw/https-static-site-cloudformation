8c0be0f31bb6d94e77b28d4a1437b0da78af1972
Andrew Watson <watson.arw@gmail.com>
2025-08-06 21:58:36 +1000

    Add warning message for conflicting flags
    Will warn about conflicting flags, but not exit
    
    e.g. ./provision -b foo --bucket-name bar "example.com" will print a warning

--- lib/_checks.lib.sh
+++ lib/_checks.lib.sh
@@ -4,11 +4,32 @@ command_exists() {
   type "$1" &>/dev/null
 }
 
+check_dependencies() {
+  if ! command_exists aws; then
+    style red <<-EOF
+You need aws-cli to run this!
+For instructions on how to install it, see:
+https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-set-up.html
+EOF
+    exit 1
+  fi
+}
+
 require_argument() {
   local option_name="$1"
   local option_value="$2"
   if [[ -z "$option_value" || "$option_value" =~ ^- ]]; then
-    echo "Error: Argument for $option_name is missing or invalid" >&2
+    style red "Error: Argument for $option_name is missing or invalid" >&2
     exit 1
+  fi
+}
+
+warn_if_conflict() {
+  local arg_name="$1"
+  local existing_value="$2"
+  local custom_message="${3:-}"
+
+  if [ -n "$existing_value" ]; then
+    style yellow "Warning: ${custom_message:-$arg_name specified multiple times. Using last value.}" >&2
   fi
 }
